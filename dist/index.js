17b2aa6c7250d38c6eb92ffeec456e3d260e4fac
Olzie-12 <oliver.bolter@virginmedia.com>
2024-02-13 04:40:41 +0000

    f

--- dist/index.js
+++ dist/index.js
@@ -6783,81 +6783,33 @@ var __importStar = (this && this.__importStar) || function (mod) {
     __setModuleDefault(result, mod);
     return result;
 };
-var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
-    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
-    return new (P || (P = Promise))(function (resolve, reject) {
-        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
-        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
-        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
-        step((generator = generator.apply(thisArg, _arguments || [])).next());
-    });
-};
-var __generator = (this && this.__generator) || function (thisArg, body) {
-    var _ = { label: 0, sent: function() { if (t[0] & 1) throw t[1]; return t[1]; }, trys: [], ops: [] }, f, y, t, g;
-    return g = { next: verb(0), "throw": verb(1), "return": verb(2) }, typeof Symbol === "function" && (g[Symbol.iterator] = function() { return this; }), g;
-    function verb(n) { return function (v) { return step([n, v]); }; }
-    function step(op) {
-        if (f) throw new TypeError("Generator is already executing.");
-        while (g && (g = 0, op[0] && (_ = 0)), _) try {
-            if (f = 1, y && (t = op[0] & 2 ? y["return"] : op[0] ? y["throw"] || ((t = y["return"]) && t.call(y), 0) : y.next) && !(t = t.call(y, op[1])).done) return t;
-            if (y = 0, t) op = [op[0] & 2, t.value];
-            switch (op[0]) {
-                case 0: case 1: t = op; break;
-                case 4: _.label++; return { value: op[1], done: false };
-                case 5: _.label++; y = op[1]; op = [0]; continue;
-                case 7: op = _.ops.pop(); _.trys.pop(); continue;
-                default:
-                    if (!(t = _.trys, t = t.length > 0 && t[t.length - 1]) && (op[0] === 6 || op[0] === 2)) { _ = 0; continue; }
-                    if (op[0] === 3 && (!t || (op[1] > t[0] && op[1] < t[3]))) { _.label = op[1]; break; }
-                    if (op[0] === 6 && _.label < t[1]) { _.label = t[1]; t = op; break; }
-                    if (t && _.label < t[2]) { _.label = t[2]; _.ops.push(op); break; }
-                    if (t[2]) _.ops.pop();
-                    _.trys.pop(); continue;
-            }
-            op = body.call(thisArg, _);
-        } catch (e) { op = [6, e]; y = 0; } finally { f = t = 0; }
-        if (op[0] & 5) throw op[1]; return { value: op[0] ? op[1] : void 0, done: true };
+Object.defineProperty(exports, "__esModule", ({ value: true }));
+const core = __importStar(__nccwpck_require__(2186));
+const ftp_deploy_1 = __nccwpck_require__(157);
+const parse_1 = __nccwpck_require__(6089);
+async function runDeployment() {
+    try {
+        const args = {
+            server: core.getInput("server", { required: true }),
+            username: core.getInput("username", { required: true }),
+            password: core.getInput("password", { required: true }),
+            port: (0, parse_1.optionalInt)("port", core.getInput("port")),
+            protocol: (0, parse_1.optionalProtocol)("protocol", core.getInput("protocol")),
+            "local-dir": (0, parse_1.optionalString)(core.getInput("local-dir")),
+            "server-dir": (0, parse_1.optionalString)(core.getInput("server-dir")),
+            "state-name": (0, parse_1.optionalString)(core.getInput("state-name")),
+            "dry-run": (0, parse_1.optionalBoolean)("dry-run", core.getInput("dry-run")),
+            "dangerous-clean-slate": (0, parse_1.optionalBoolean)("dangerous-clean-slate", core.getInput("dangerous-clean-slate")),
+            "exclude": (0, parse_1.optionalStringArray)("exclude", core.getMultilineInput("exclude")),
+            "log-level": (0, parse_1.optionalLogLevel)("log-level", core.getInput("log-level")),
+            "security": (0, parse_1.optionalSecurity)("security", core.getInput("security")),
+            "timeout": (0, parse_1.optionalInt)("timeout", core.getInput("timeout"))
+        };
+        await (0, ftp_deploy_1.deploy)(args);
     }
-};
-exports.__esModule = true;
-var core = __importStar(__nccwpck_require__(2186));
-var ftp_deploy_1 = __nccwpck_require__(157);
-var parse_1 = __nccwpck_require__(6089);
-function runDeployment() {
-    return __awaiter(this, void 0, void 0, function () {
-        var args, error_1;
-        return __generator(this, function (_a) {
-            switch (_a.label) {
-                case 0:
-                    _a.trys.push([0, 2, , 3]);
-                    args = {
-                        server: core.getInput("server", { required: true }),
-                        username: core.getInput("username", { required: true }),
-                        password: core.getInput("password", { required: true }),
-                        port: (0, parse_1.optionalInt)("port", core.getInput("port")),
-                        protocol: (0, parse_1.optionalProtocol)("protocol", core.getInput("protocol")),
-                        "local-dir": (0, parse_1.optionalString)(core.getInput("local-dir")),
-                        "server-dir": (0, parse_1.optionalString)(core.getInput("server-dir")),
-                        "state-name": (0, parse_1.optionalString)(core.getInput("state-name")),
-                        "dry-run": (0, parse_1.optionalBoolean)("dry-run", core.getInput("dry-run")),
-                        "dangerous-clean-slate": (0, parse_1.optionalBoolean)("dangerous-clean-slate", core.getInput("dangerous-clean-slate")),
-                        "exclude": (0, parse_1.optionalStringArray)("exclude", core.getMultilineInput("exclude")),
-                        "log-level": (0, parse_1.optionalLogLevel)("log-level", core.getInput("log-level")),
-                        "security": (0, parse_1.optionalSecurity)("security", core.getInput("security")),
-                        "timeout": (0, parse_1.optionalInt)("timeout", core.getInput("timeout"))
-                    };
-                    return [4 /*yield*/, (0, ftp_deploy_1.deploy)(args)];
-                case 1:
-                    _a.sent();
-                    return [3 /*break*/, 3];
-                case 2:
-                    error_1 = _a.sent();
-                    core.setFailed(error_1);
-                    return [3 /*break*/, 3];
-                case 3: return [2 /*return*/];
-            }
-        });
-    });
+    catch (error) {
+        core.setFailed(error);
+    }
 }
 runDeployment();
 
@@ -6869,7 +6821,7 @@ runDeployment();
 
 "use strict";
 
-exports.__esModule = true;
+Object.defineProperty(exports, "__esModule", ({ value: true }));
 exports.optionalStringArray = exports.optionalInt = exports.optionalSecurity = exports.optionalLogLevel = exports.optionalProtocol = exports.optionalBoolean = exports.optionalString = void 0;
 function optionalString(rawValue) {
     if (rawValue.length === 0) {
@@ -6882,21 +6834,21 @@ function optionalBoolean(argumentName, rawValue) {
     if (rawValue.length === 0) {
         return undefined;
     }
-    var cleanValue = rawValue.toLowerCase();
+    const cleanValue = rawValue.toLowerCase();
     if (cleanValue === "true") {
         return true;
     }
     if (cleanValue === "false") {
         return false;
     }
-    throw new Error("".concat(argumentName, ": invalid parameter - please use a boolean, you provided \"").concat(rawValue, "\". Try true or false instead."));
+    throw new Error(`${argumentName}: invalid parameter - please use a boolean, you provided "${rawValue}". Try true or false instead.`);
 }
 exports.optionalBoolean = optionalBoolean;
 function optionalProtocol(argumentName, rawValue) {
     if (rawValue.length === 0) {
         return undefined;
     }
-    var cleanValue = rawValue.toLowerCase();
+    const cleanValue = rawValue.toLowerCase();
     if (cleanValue === "ftp") {
         return "ftp";
     }
@@ -6906,14 +6858,14 @@ function optionalProtocol(argumentName, rawValue) {
     if (cleanValue === "sftp") {
         return "sftp";
     }
-    throw new Error("".concat(argumentName, ": invalid parameter - you provided \"").concat(rawValue, "\". Try \"ftp\", \"ftps\", or \"ftps-legacy\" instead."));
+    throw new Error(`${argumentName}: invalid parameter - you provided "${rawValue}". Try "ftp", "ftps", or "ftps-legacy" instead.`);
 }
 exports.optionalProtocol = optionalProtocol;
 function optionalLogLevel(argumentName, rawValue) {
     if (rawValue.length === 0) {
         return undefined;
     }
-    var cleanValue = rawValue.toLowerCase();
+    const cleanValue = rawValue.toLowerCase();
     if (cleanValue === "minimal") {
         return "minimal";
     }
@@ -6923,32 +6875,32 @@ function optionalLogLevel(argumentName, rawValue) {
     if (cleanValue === "verbose") {
         return "verbose";
     }
-    throw new Error("".concat(argumentName, ": invalid parameter - you provided \"").concat(rawValue, "\". Try \"minimal\", \"standard\", or \"verbose\" instead."));
+    throw new Error(`${argumentName}: invalid parameter - you provided "${rawValue}". Try "minimal", "standard", or "verbose" instead.`);
 }
 exports.optionalLogLevel = optionalLogLevel;
 function optionalSecurity(argumentName, rawValue) {
     if (rawValue.length === 0) {
         return undefined;
     }
-    var cleanValue = rawValue.toLowerCase();
+    const cleanValue = rawValue.toLowerCase();
     if (cleanValue === "loose") {
         return "loose";
     }
     if (cleanValue === "strict") {
         return "strict";
     }
-    throw new Error("".concat(argumentName, ": invalid parameter - you provided \"").concat(rawValue, "\". Try \"loose\" or \"strict\" instead."));
+    throw new Error(`${argumentName}: invalid parameter - you provided "${rawValue}". Try "loose" or "strict" instead.`);
 }
 exports.optionalSecurity = optionalSecurity;
 function optionalInt(argumentName, rawValue) {
     if (rawValue.length === 0) {
         return undefined;
     }
-    var valueAsNumber = parseFloat(rawValue);
+    const valueAsNumber = parseFloat(rawValue);
     if (Number.isInteger(valueAsNumber)) {
         return valueAsNumber;
     }
-    throw new Error("".concat(argumentName, ": invalid parameter - you provided \"").concat(rawValue, "\". Try a whole number (no decimals) instead like 1234"));
+    throw new Error(`${argumentName}: invalid parameter - you provided "${rawValue}". Try a whole number (no decimals) instead like 1234`);
 }
 exports.optionalInt = optionalInt;
 function optionalStringArray(argumentName, rawValue) {
@@ -6956,7 +6908,7 @@ function optionalStringArray(argumentName, rawValue) {
         return undefined;
     }
     if (typeof rawValue === "string") {
-        throw new Error("".concat(argumentName, ": invalid parameter - you provided \"").concat(rawValue, "\". This option expects an list in the EXACT format described in the readme"));
+        throw new Error(`${argumentName}: invalid parameter - you provided "${rawValue}". This option expects an list in the EXACT format described in the readme`);
     }
     return rawValue;
 }
